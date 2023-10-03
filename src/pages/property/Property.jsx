407f1b4d9e32e6a16db37823765c7b7354085e3e
As7500 <85102814+As7500@users.noreply.github.com>
2023-10-03 21:15:15 +0545

    intern

---
+++ src/pages/property/Property.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import PropertyDetails from './PropertyDetails';
+
+const Property = () => {
+
+  const {property_id} = useParams();
+  const [propertyData, setPropertyData] = useState();
+
+  // get local Storage data
+  const loadSingleProperty = () => {
+    const data = JSON.parse(localStorage.getItem('data'))
+    if (data) {
+       const single = data.find((item)=> item.id === property_id);
+       setPropertyData(single)
+    }
+ }
+
+   useEffect(() => {
+    loadSingleProperty()
+  },[])
+
+  return (
+    <div className='py-20 bg-neutral-100'>
+      <div className='container mx-auto xl:px-20 md:px-10 sm:px-2 px-4'>
+          <PropertyDetails property={propertyData}/>
+      </div>
+    </div>
+  )
+}
+
+export default Property
